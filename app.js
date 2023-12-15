7ac876c4044b0b5b594b1c9d11cf5f15a4a0a7c2
Chinedu <chinedunnaemeka2@gmail.com>
2023-12-15 22:32:25 +0100

    modified:   .gitignore         deleted:    public/index.html and files and view folder created

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express()
+
+//CSS
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+
+//EJS
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
+//routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
