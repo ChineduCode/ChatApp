a760ee3cee2fee220ed349ff57795901d6b3c70d
Chinedu <chinedunnaemeka2@gmail.com>
2023-12-16 02:00:54 +0100

    modified:   app.js         modified:   public/css/main.css         modified:   public/css/main.css.map         modified:   public/css/main.scss         modified:   views/layout.ejs and partial folder created

--- app.js
+++ app.js
@@ -16,4 +16,4 @@ app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=> console.log(`server running on port ${PORT}`))
+app.listen(PORT, ()=> console.log(`server running on port ${PORT} ...`))
